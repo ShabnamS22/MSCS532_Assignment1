b4c24ed26ceb868e4e25d275136332cc3e317ea7
ShabnamS22 <sshaikh29440@ucumberlands.edu>
2026-01-19 00:56:24 -0800

    Add insertion sort implementation

---
+++ insertion_sort.py
@@ -0,0 +1,22 @@
+def insertion_sort_desc(arr):
+    """
+    Sorts an array in monotonically decreasng order
+    usin the insertion sort algorithm
+    """
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+            arr[j + 1] = key
+
+    if __name__ == "__main__":
+        data = [31, 41, 59, 26, 41, 58]
+        print("Original array:", data)
+
+        insertion_sort_desc(data)
+
+        print("Sorted array (descending):", data)
